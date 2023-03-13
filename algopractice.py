1eadbcc3465e62f3d0a107be454de0ee164baf8c
StiltonBree <nruth@g.clemson.edu>
2023-03-13 16:08:37 -0700

    Made Simple Collatz Algo

--- algopractice.py
+++ algopractice.py
@@ -2,3 +2,13 @@
 
 #find steps to 1. If n is even, divide by 2, if n is odd, multiply by 3 and add 1
 
+n = int(input("Enter a number: "))
+count = 0
+while n > 1:
+    if (n % 2 == 0):
+        n = n/2
+    else:
+        n = (n * 3) + 1
+    count+=1
+
+print("It takes " + str(count) + " steps to get to 1")       
